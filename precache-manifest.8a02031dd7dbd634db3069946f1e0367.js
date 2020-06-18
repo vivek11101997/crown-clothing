12f758ef2b0b95be9c0cd4ed7a8519836d109c33
vivek chaudhari <vivek11101997@gmail.com>
2020-06-18 15:34:46 +0530

    Updates

---
+++ precache-manifest.8a02031dd7dbd634db3069946f1e0367.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1087bccc66faadfdaa9bb5871b05924f",
+    "url": "/crown-clothing/index.html"
+  },
+  {
+    "revision": "0a7161ce4185ccf3041d",
+    "url": "/crown-clothing/static/css/main.20434ec2.chunk.css"
+  },
+  {
+    "revision": "5e636ae16d6c8bf08a1b",
+    "url": "/crown-clothing/static/js/2.dfd06d6c.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/crown-clothing/static/js/2.dfd06d6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a7161ce4185ccf3041d",
+    "url": "/crown-clothing/static/js/main.20e540c7.chunk.js"
+  },
+  {
+    "revision": "63bc4cc590a717ec16e4",
+    "url": "/crown-clothing/static/js/runtime-main.bfc44cf4.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crown-clothing/static/media/crown.ed28accb.svg"
+  }
+]);
